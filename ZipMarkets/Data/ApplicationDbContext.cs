5d56dae288f6d72b7ad9b86c4b4b169f126fc973
Jon <durrjp1@gmail.com>
2020-07-27 12:53:10 -0500

    creating some models, but revisiting dB

--- ZipMarkets/Data/ApplicationDbContext.cs
+++ ZipMarkets/Data/ApplicationDbContext.cs
@@ -9,5 +9,6 @@ namespace ZipMarkets.Data
     public class ApplicationDbContext: DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
     }
 }
---
+++ ZipMarkets/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZipMarkets.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirebaseUserId { get; set; }
+        public string DisplayName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int HomeZip { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public int UserTypeId { get; set; }
+        public UserType UserType { get; set; }
+        public int MinHomePrice { get; set; }
+        public int MaxHomePrice { get; set; }
+    }
+}
