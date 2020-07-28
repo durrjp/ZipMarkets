9bc783998a7a2e8059f1977464c8df5491b50db4
Jon <durrjp1@gmail.com>
2020-07-27 20:46:02 -0500

    user controller

--- ZipMarkets/Controllers/UserController.cs
+++ ZipMarkets/Controllers/UserController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ZipMarkets.Data;
+using ZipMarkets.Models;
+using ZipMarkets.Repositories;
 
 namespace ZipMarkets.Controllers
 {
@@ -11,5 +14,47 @@ namespace ZipMarkets.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly UserRepository _userRepository;
+        public UserController(ApplicationDbContext context)
+        {
+            _userRepository = new UserRepository(context);
+        }
+
+        [HttpGet("{firebaseUserId}")]
+        public IActionResult GetUser(string firebaseUserId)
+        {
+            return Ok(_userRepository.GetByFirebaseUserId(firebaseUserId));
+        }
+
+        [HttpGet("id/{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            return Ok(_userRepository.GetUserById(id));
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_userRepository.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult Post(User user)
+        {
+            user.CreateDateTime = DateTime.Now;
+            user.UserTypeId = 2;
+            _userRepository.Add(user);
+            return CreatedAtAction(
+                nameof(GetUser),
+                new { firebaseUserId = user.FirebaseUserId },
+                user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(User user)
+        {
+            _userRepository.Update(user);
+            return Ok(user);
+        }
     }
 }
