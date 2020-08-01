70ccd11189422150f814e589baad04fef7a31311
Jon <durrjp1@gmail.com>
2020-08-01 16:56:05 -0500

    building front-end for feed

--- ZipMarkets/client/src/components/zipDetails/Feed.js
+++ ZipMarkets/client/src/components/zipDetails/Feed.js
@@ -1,11 +1,11 @@
 import React, { useContext, useState } from "react"
 import { MessageContext } from "../../providers/MessageProvider"
-import { Input } from "reactstrap";
+import { Input, Button } from "reactstrap";
 
 export default function Feed({oneZip}) {
     const {addMessage, deleteMessage} = useContext(MessageContext)
     const [content, setContent] = useState();
-    const [categoryId, setCartegoryId] = useState()
+    const [categoryId, setCategoryId] = useState()
 
     const currentUser = JSON.parse(sessionStorage.getItem("user"));
 
@@ -24,12 +24,27 @@ export default function Feed({oneZip}) {
     }
     return (
         <>
-        <div>
+        <div className="messages-container">
+            
+        </div>
+        <div className="messageSubmit-container">
             <Input
-                
+                type="text"
+                id="newMessage"
+                placeholder="Enter message here"
+                onInput={(e) => setContent(e.target.value)}
             >
-
             </Input>
+            <Button
+                type="submit"
+                size="md"
+                onClick={(e) => {
+                    e.preventDefault()
+                    saveMessage()
+                }}
+            >
+            Send
+            </Button>
         </div>
         </>
     )
