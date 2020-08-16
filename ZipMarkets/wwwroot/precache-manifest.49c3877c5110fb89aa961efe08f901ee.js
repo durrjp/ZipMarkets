1c7938cc28de538f9101cd1995e2f3d9dc89d0a8
Jon <durrjp1@gmail.com>
2020-08-16 16:41:58 -0500

    deploying fixed mortgage rate graph and styling

---
+++ ZipMarkets/wwwroot/precache-manifest.49c3877c5110fb89aa961efe08f901ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4852db17fc84126a3a1d7edf34620f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49085f20085c4c0a794e",
+    "url": "/static/css/2.75d792e8.chunk.css"
+  },
+  {
+    "revision": "56a05e85841e273f3a4d",
+    "url": "/static/css/main.01988500.chunk.css"
+  },
+  {
+    "revision": "49085f20085c4c0a794e",
+    "url": "/static/js/2.fb1eda9a.chunk.js"
+  },
+  {
+    "revision": "eb7062dc80596e2bf145ee2db2c06151",
+    "url": "/static/js/2.fb1eda9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a05e85841e273f3a4d",
+    "url": "/static/js/main.86c4623d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "730b47f5e71f60a25be88654f68e13f0",
+    "url": "/static/media/Pinned.730b47f5.svg"
+  }
+]);
