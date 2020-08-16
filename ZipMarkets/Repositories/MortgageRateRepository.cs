28f53f2b0726bd34006dba0252e69c1a64e649c7
Jon <durrjp1@gmail.com>
2020-08-16 14:43:49 -0500

    building backend for us mortgage rates last 20 years

---
+++ ZipMarkets/Repositories/MortgageRateRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZipMarkets.Data;
+
+namespace ZipMarkets.Repositories
+{
+    public class MortgageRateRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MortgageRateRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
